01b8507117a27b481d0405fc183e92e55e68225a
SamerNassim <samernassim@std.mans.edu.eg>
2022-03-14 13:11:34 +0200

    Done

--- Array.diff.js
+++ Array.diff.js
@@ -1,10 +1,15 @@
-function arrayDiff(a, b) {
-  let result = [];
-  for (let i of a) {
-    if (!b.includes(i)) {
-      result.push(i);
-    }
-  }
+// function arrayDiff(a, b) {
+//   let result = [];
+//   for (let i of a) {
+//     if (!b.includes(i)) {
+//       result.push(i);
+//     }
+//   }
 
-  return result;
+//   return result;
+// }
+
+// Second Solution
+function arrayDiff(a, b) {
+  return a.filter((e) => !b.includes(e));
 }
