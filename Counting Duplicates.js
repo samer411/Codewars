dce5cef9812d97957c17966b1fd4faaf9b0432c9
SamerNassim <samernassim@std.mans.edu.eg>
2022-03-16 08:18:46 +0200

    16/3/2022

--- Counting Duplicates.js
+++ Counting Duplicates.js
@@ -1,1 +1,8 @@
-function duplicateCount(text) {}
+function duplicateCount(text) {
+  text = text.toLowerCase();
+  let result = 0;
+  text.filter((e) => if(text.count(e) > 1)
+  { ++result });
+  return result;
+}
+console.log(duplicateCount("abBcde"));
