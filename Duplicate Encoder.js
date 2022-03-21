9418447c239835dba1f76dee513afd7ba8fd1339
SamerNassim <samernassim@std.mans.edu.eg>
2022-03-21 19:34:02 +0200

    21/3/2022

--- Duplicate Encoder.js
+++ Duplicate Encoder.js
@@ -1,8 +1,32 @@
+// function duplicateEncode(word) {
+//   word = word.toLowerCase();
+//   let duplicates = [];
+//   for (let i = 0; i < word.length; i++) {
+//     for (let j = i + 1; j < word.length; j++) {
+//       if (word[i] == word[j]) {
+//         duplicates.push(word[i]);
+//       }
+//     }
+//   }
+//   let result = [];
+//   for (let i of word) {
+//     if (duplicates.includes(i)) {
+//       result.push(")");
+//     } else {
+//       result.push("(");
+//     }
+//   }
+//   return String(result.join(""));
+// }
+
+// ************ Second Solution **********
 function duplicateEncode(word) {
   return word
     .toLowerCase()
     .split("")
-    .sort()
-    .match(/(.)\1+/g);
+    .map(function(e, i, a) {
+      return a.indexOf(e) == a.lastIndexOf(e) ? "(" : ")";
+    })
+    .join("");
 }
-console.log(duplicateEncode("Success"));
+console.log(duplicateEncode("recede"));
