940244eae8654d4e8b5da8cf12804d8fcac8849b
SamerNassim <samernassim@std.mans.edu.eg>
2022-03-30 11:30:20 +0200

    30/3/2022

--- Persistent Bugger.js
+++ Persistent Bugger.js
@@ -6,13 +6,11 @@ function persistence(num) {
 
     while (temp > 0) {
       let digit = temp % 10;
-      console.log(digit);
       temp = Math.floor(temp / 10);
-      console.log(temp);
       num *= digit;
     }
     ans++;
   }
   return ans;
 }
-console.log(persistence(999));
+console.log(persistence(99));
