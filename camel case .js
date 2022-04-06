26c4aa300eab285087c41ca0e3e041dc10f1ff0d
SamerNassim <samernassim@std.mans.edu.eg>
2022-04-06 14:56:22 +0200

    6/4/2022

--- camel case .js
+++ camel case .js
@@ -1,6 +1,20 @@
 function toCamelCase(str) {
   if (str == "") return "";
-  str.split();
+  else {
+    let r = str.split("_");
+    if (r.length <= 1) {
+      r = str.split("-");
+    }
+    for (let i = 1; i < r.length; i++) {
+      let ch = str[i][0].toUpperCase();
+      console.log(str[i]);
+      console.log(ch);
+      console.log(str[i].slice(1));
+      str[i] = ch + str[i].slice(1);
+      console.log(str[i]);
+    }
+    return r.join("");
+  }
 }
 
 console.log(toCamelCase("the_stealth_warrior"));
