d8210836cae19d309dc48b110769c708429b83fb
SamerNassim <samernassim@std.mans.edu.eg>
2022-04-09 16:44:18 +0200

    9/4/2022

--- Tribonacci Sequence.js
+++ Tribonacci Sequence.js
@@ -1,1 +1,11 @@
-function tribonacci(signature, n) {}
+function tribonacci(signature, n) {
+    if (n=0) return []
+    
+    else{
+        for(let i =0;i<n;i++){
+            let next;
+            next =
+        }
+    }
+    
+}
