24cdbe018e0989d606fb4bb911525a8c83db7c23
SamerNassim <samernassim@std.mans.edu.eg>
2022-04-13 05:53:14 +0200

    13/4/2022

--- Tribonacci Sequence.js
+++ Tribonacci Sequence.js
@@ -1,16 +1,12 @@
 function tribonacci(signature, n) {
-  if ((n = 0)) return [];
-  else {
-    let result = signature;
-    for (let i = 0; i < n - 3; i++) {
-      let next_item;
-      for (let j = -1; j > -4; j--) {
-        next_item += result[j];
-      }
-      console.log(next_item);
-      result.push(next_item);
+  let result = signature;
+  for (let i = 0; i < n - 3; i++) {
+    let next_item = 0;
+    for (let j = 1; j > 4; j++) {
+      // next_item += result[result.length - j];
+      // console.log(next_item);
+      console.log(result[result.length() - j]);
     }
-    return result;
   }
 }
 console.log(tribonacci([1, 1, 1], 10));
